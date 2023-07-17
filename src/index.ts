bf082092b65a2523ee03e00e6b22fc6def323003
itsvikasdwivedi <vickyserver22@gmail.com>
2023-07-17 23:37:58 +0530

    UPD

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true
+      // debug: true
     }
   },
   scene:[PreloadScene,PlayScene]
--- webpack.common.js
+++ webpack.common.js
@@ -52,10 +52,10 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'assets/**/*'),
-          to: path.resolve(__dirname, 'build')
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'build/assets')
         }
-      ]
+      ],
     })
   ]
 };
